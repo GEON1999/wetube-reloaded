21c6662fad90a4cf568b4c64b4005941501bc63b
GEON1999 <101832525+GEON1999@users.noreply.github.com>
2022-05-26 08:55:22 +0900

    loadedmetadata issue3

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -140,6 +140,6 @@ videoContainer.addEventListener("mouseleave", handleMouseLeave);
 document.addEventListener("keydown", handleKeydown);
 fullScreenBtn.addEventListener("click", handleFullScreen);
 
-if (vid.readyState >= 4) {
+if (video.readyState >= 4) {
     handleLoadedMetaData();
   }
