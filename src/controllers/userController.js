17dde904abc097d34c0ab4de6c71cf5603a837de
GEON1999 <101832525+GEON1999@users.noreply.github.com>
2022-05-25 17:32:28 +0900

    s3_upload

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -155,7 +155,7 @@ export const postEdit = async(req, res) => {
         file,
     } = req;
     const updateUser = await User.findByIdAndUpdate(_id, {
-        avatarUrl: file ? file.path : avatarUrl,        
+        avatarUrl: file ? file.location : avatarUrl,        
         name,
         email,
         username,
